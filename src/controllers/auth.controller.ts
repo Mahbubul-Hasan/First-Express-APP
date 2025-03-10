8f1ad9d58f905ee16d8e5401c8ba32d20e763b14
Mahbubul Hasan <mahbubul161@gmail.com>
2025-03-10 21:59:34 +0600

    Refactor AuthController to remove static methods for signup, signin, and updateProfilePicture

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,19 +3,19 @@ import ResponseHandler from "../utils/response.handler.js";
 import AuthService from "../services/auth.service.js";
 
 class AuthController {
-    static signup(req: Request, res: Response) {
+    signup(req: Request, res: Response) {
         ResponseHandler.handlerAsyncResponse(res, async () => {
             return await new AuthService().signup(req);
         });
     }
 
-    static signin(req: Request, res: Response) {
+    signin(req: Request, res: Response) {
         ResponseHandler.handlerAsyncResponse(res, async () => {
             return await new AuthService().signin(req);
         });
     }
 
-    static async updateProfilePicture(req: Request, res: Response) {
+    async updateProfilePicture(req: Request, res: Response) {
         ResponseHandler.handlerAsyncResponse(res, async () => {
             return await new AuthService().updateProfilePicture(req);
         });
