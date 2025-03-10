bfa6f37c37cb02f76da6ca34678297175af57b25
Mahbubul Hasan <mahbubul161@gmail.com>
2025-03-10 22:20:21 +0600

    Add validation middleware for signup route using Zod

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import AuthController from "../../controllers/auth.controller.js";
 import fileUpload from "../../utils/file.upload.js";
+import validate from "../../validations/validation.js";
+import signupSchema from "../../validations/signup.validation.js";
 
 const router = express.Router();
 
 const authController = new AuthController();
 
-router.post("/sign-up", authController.signup);
+router.post("/sign-up", validate(signupSchema), authController.signup);
 router.post("/sign-in", authController.signin);
 router.post("/update-profile-picture", fileUpload.single("image"), authController.updateProfilePicture);
 
