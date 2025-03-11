cb4b7c5f2f9c319c88dd50de417f7bf31dec794b
Mahbubul Hasan <mahbubul161@gmail.com>
2025-03-12 02:30:00 +0600

    Add category model and type definitions for MongoDB integration

---
+++ src/models/category.model.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { CategoryT } from "../types/category.type.js";
+
+const categorySchema = new mongoose.Schema<CategoryT>(
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+            minlength: [2, "Title must be at least 2 characters"],
+            maxlength: [10, "Title cannot exceed 10 characters"],
+        },
+        description: { type: String },
+    },
+    { timestamps: true }
+);
+
+export const Category = mongoose.model<CategoryT>("categories", categorySchema);
