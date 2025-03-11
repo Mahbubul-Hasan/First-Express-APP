18b22e6041a597a125d4087b8e831e75425c5287
Mahbubul Hasan <mahbubul161@gmail.com>
2025-03-12 03:27:23 +0600

    Refactor database connection to use connectDB utility function

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import routerV1 from "./routes/v1/index.js";
+import { connectDB } from "./utils/database.js";
 
 const app = express();
 dotenv.config();
 
-mongoose
-    .connect(process.env.MONGODB_CONNECTION_STRING, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("Connected to mongodb"))
-    .catch((err) => console.log(err));
+await connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
