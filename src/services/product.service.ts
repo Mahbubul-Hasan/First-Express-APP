6172132bfc9702d32c8617ac16891d66942ef49d
Mahbubul Hasan <mahbubul161@gmail.com>
2025-03-12 04:20:00 +0600

    Remove unused imports from product controller and service files

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,4 +1,3 @@
-import { Request, Response } from "express";
 import { HTTP_STATUS_CODES } from "../constants/response.codes.js";
 import { responseFormat } from "../utils/response.formatter.js";
 import { Product } from "../models/product.model.js";
