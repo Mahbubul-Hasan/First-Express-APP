3791c7640846a08a1ec39015fb92b5a9a55b4476
Mahbubul Hasan <mahbubul161@gmail.com>
2025-03-15 03:57:45 +0600

    Refactor authenticate middleware to use custom request type and include NextFunction for improved type safety

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,10 +1,11 @@
-import { Response } from "express";
+import { NextFunction, Response } from "express";
 import jwt from "jsonwebtoken";
 import { HTTP_STATUS_CODES, HTTP_STATUS_MESSAGES } from "../constants/response.codes.js";
 import { responseFormat } from "../utils/response.formatter.js";
 import { User } from "../models/user.model.js";
+import { RequestX } from "../types/custom.request.js";
 
-export const authenticate = async (req, res: Response, next) => {
+export const authenticate = async (req: RequestX, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         const result = responseFormat(false, HTTP_STATUS_MESSAGES.UNAUTHORIZED, {}, HTTP_STATUS_CODES.UNAUTHORIZED);
