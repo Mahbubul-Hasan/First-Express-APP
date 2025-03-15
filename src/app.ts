31e1c4b0a1179e2262cbaabeff61e9dfb5c3b02e
Mahbubul Hasan <mahbubul161@gmail.com>
2025-03-15 14:07:37 +0600

    Add CORS and Morgan middleware for improved request handling and logging

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
 import routerV1 from "./routes/v1/index.js";
 import { connectDB } from "./utils/database.js";
 
@@ -11,6 +12,10 @@ await connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use(morgan("dev"));
 
 app.use("/v1", routerV1);
 
